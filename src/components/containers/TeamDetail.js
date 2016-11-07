1d439da051bdbd747e6c7c4afac6b15b921d8d32
fullstack360 <info@fullstack360.com>
2016-11-06 20:54:16 -0500

    removed image upload from team detail

--- src/components/containers/TeamDetail.js
+++ src/components/containers/TeamDetail.js
@@ -1,32 +1,9 @@
 import React, { Component } from 'react'
-import Dropzone from 'react-dropzone'
 import { connect } from 'react-redux'
-import { APIManager } from '../../utils'
-import store from '../../stores/store'
 import actions from '../../actions/actions'
 import { Map } from '../view'
 
 class TeamDetail extends Component {
-
-	uploadImage(files){
-		APIManager.upload(files[0], (err, image) => {
-			if (err){
-				alert(err)
-				return
-			}
-
-			const team = this.props.teams[this.props.slug]
-			let updated = Object.assign({}, team)
-			updated['image'] = image.address
-
-			const url = '/api/team/'+updated.id
-			APIManager.handlePut(url, updated, (error, response) => {
-				console.log(JSON.stringify(response))
-				const result = response.result
-				store.currentStore().dispatch(actions.teamsReceived([result]))
-			})
-		})
-	}
 
 	render(){
 		const team = this.props.teams[this.props.slug]
@@ -53,11 +30,6 @@ class TeamDetail extends Component {
 							<a href={'/scrape?team='+team.id}>Scrape</a>
 							<br />
 							<img src={team.image+'=s220'} />
-							<br /><br />
-
-				            <Dropzone onDrop={this.uploadImage.bind(this)}>
-				              	Upload Image Here<br />
-				            </Dropzone>
 
 						</div>
 					</div>
