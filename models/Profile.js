ea74d94f93bd27951c2e5f0675c279e2ce15ff09
fullstack360 <info@fullstack360.com>
2016-11-14 01:01:54 -0500

    add social to profile

--- models/Profile.js
+++ models/Profile.js
@@ -6,6 +6,7 @@ var ProfileSchema = new mongoose.Schema({
 	email: {type:String, trim:true, lowercase:true, default:''},
 	credits: {type:Number, default:0}, // first 3 are free
 	password: {type:String, trim:true, default:''},
+	social: {type:mongoose.Schema.Types.Mixed, default:{}},
 	timestamp: {type:Date, default:Date.now}
 })
 
@@ -15,6 +16,7 @@ ProfileSchema.methods.summary = function(){
 		image: this.image,
 		email: this.email,
 		credits: this.credits,
+		social: this.social,
 		timestamp: this.timestamp,
 		id: this._id.toString()
 	}
