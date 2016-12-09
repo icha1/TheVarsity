1c8e28b9fb608662af13bc3a55c51078f0b6e596
fullstack360 <info@fullstack360.com>
2016-12-08 23:51:12 -0500

    join and leave districts

--- src/desktop/containers/District.js
+++ src/desktop/containers/District.js
@@ -28,13 +28,47 @@ class District extends Component {
 
 	joinDistrict(event){
 		event.preventDefault()
-		console.log('Join District')
+		const user = this.props.user
+		if (user == null)
+			return
+
+		const district = this.props.session.currentDistrict
+		let districts = user.districts
+		if (districts.indexOf(district.id) != -1)
+			return
+
+		districts.push(district.id)
+		this.props.updateProfile(user, {districts: districts})
+	}
+
+	leaveDistrict(event){
+		event.preventDefault()
+		const user = this.props.user
+		if (user == null)
+			return
+
+		const district = this.props.session.currentDistrict
+		let districts = user.districts
+		const index = districts.indexOf(district.id)
+		if (index == -1)
+			return
+
+		districts.splice(index, 1)
+		this.props.updateProfile(user, {districts: districts})
 	}
 
 	render(){
 		const style = styles.district
 		const district = this.props.session.currentDistrict
 		const selectedFeed = this.props.session.selectedFeed
+
+		let btn = null
+		if (this.props.user != null){
+			if (this.props.user.districts.indexOf(district.id) == -1)			
+				btn = <button onClick={this.joinDistrict.bind(this)} style={{float:'right'}}>Join</button>
+			else
+				btn = <button onClick={this.leaveDistrict.bind(this)} style={{float:'right'}}>Leave District</button>
+		}
 
 		const list = this.state.feedOptions.map((feed, i) => {
 			let selected = (selectedFeed == feed.name) ? style.selected : style.unselected
@@ -53,7 +87,7 @@ class District extends Component {
 					<div style={{minHeight:140}}>
 						<div style={style.body}>
 							<span style={style.header}>{district.name}</span>
-							<button onClick={this.joinDistrict.bind(this)} style={{float:'right'}}>Join</button>
+							{ btn }
 							<hr />
 							<img style={{width:96,float:'right'}} src={district.image+'=s120-c'} />
 							<ul style={style.list}>
@@ -84,7 +118,8 @@ const stateToProps = (state) => {
 
 const dispatchToProps = (dispatch) => {
 	return {
-		changeSelectedFeed: (feed) => dispatch(actions.selectedFeedChanged(feed))		
+		changeSelectedFeed: (feed) => dispatch(actions.selectedFeedChanged(feed)),
+		updateProfile: (profile, params) => dispatch(actions.updateProfile(profile, params))
 	}
 }
 
