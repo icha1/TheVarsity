ce62ab435eb6c9917b055d7a17e8baf1cc4be794
fullstack360 <info@fullstack360.com>
2016-12-11 02:46:21 -0500

    send screenshot to your image hosting

--- routes/phantom.js
+++ routes/phantom.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 var screenshot = require('url-to-image')
+var superagent = require('superagent')
+var path = require('path')
 
 router.get('/', function(req, res, next){
 	var url = req.query.url
@@ -8,19 +10,71 @@ router.get('/', function(req, res, next){
 	screenshot(url, 'public/images/file.png')
 	.then(function() {
 
-		res.json({
-			confirmation: 'success',
-			message: url+' screenshot saved to file.png'
+		// res.json({
+		// 	confirmation: 'success',
+		// 	message: url+' screenshot saved to file.png'
+		// })
+
+		superagent
+		.get('https://media-service.appspot.com/api/upload')
+		.query(null)
+		.set('Accept', 'application/json')
+		.end((err, response) => {
+			if (err){ 
+				res.json({
+					confirmation: 'fail',
+					message: err
+				})
+				return
+			}
+
+			if (response.body.confirmation != 'success'){
+				res.json({
+					confirmation: 'fail',
+					message: response.body.message
+				})
+	    		// completion({message:res.body.message}, null)
+	    		return
+			}
+
+	        var uploadRequest = superagent.post(response.body.upload)
+//	        uploadRequest.attach('file', file)
+
+			var filepath = path.join(__dirname, 'public', 'images/file.png').replace('routes/', '')
+			console.log('PATH == '+filepath)
+	        uploadRequest.attach('file', filepath)
+	        uploadRequest.end((err, resp) => {
+	        	if (err){
+					res.json({
+						confirmation: 'fail',
+						message: err
+					})
+
+
+			      	console.log('UPLOAD ERROR: '+JSON.stringify(err))
+//					completion(err, null)
+	              	return
+	        	}
+
+
+		      	var image = resp.body.image
+				console.log('DONE == '+JSON.stringify(resp.body))
+
+				res.json({
+					confirmation: 'success',
+					image: image
+				})
+
+//				completion(null, image)
+	        })
 		})
 
 	})
-	.catch(function(err){
-
+	.catch(function(error){
 		res.json({
 			confirmation: 'fail',
-			message: err
+			message: error
 		})
-
 	})
 
 })
