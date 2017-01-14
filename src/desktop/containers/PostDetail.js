313518d9dd5d1f47268cc4445d67680ca2cecc04
fullstack360 <info@fullstack360.com>
2017-01-13 20:48:24 -0500

    mobile comments

--- src/desktop/containers/PostDetail.js
+++ src/desktop/containers/PostDetail.js
@@ -298,8 +298,6 @@ class PostDetail extends Component {
 							<p className="lead" style={{fontSize:16, color:'#555'}} dangerouslySetInnerHTML={{__html:TextUtils.convertToHtml(post.text)}}></p>
 						</div>
 						<hr />
-						<textarea style={styles.replyBox} placeholder='Reply'></textarea>
-						<button className="button button-mini button-circle button-green">Submit Reply</button>
 					</div>					
 				</div>
 			)
@@ -411,6 +409,20 @@ class PostDetail extends Component {
 						}
 					</div>
 					{ content }
+
+					<div style={{padding:12, borderBottom:'1px solid #ddd'}}>
+						<h3 style={style.title}>Comments</h3>
+					</div>
+					{ (this.state.comments == null) ? null : this.state.comments.map((comment, i) => {
+							return <Comment key={comment.id} comment={comment} />
+						})
+					}
+
+					<div>
+						<input type="text" id="text" value={this.state.comment.text} onChange={this.updateComment.bind(this)} onKeyPress={this.submitComment.bind(this)} style={localStyle.input} placeholder="Enter Comment" />
+					</div>
+
+
 				</div>
 				{ /* end mobile UI */ }
 
