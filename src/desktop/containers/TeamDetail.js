51a1aca7ad81c43b56faba8b68e7479b4db6de15
fullstack360 <info@fullstack360.com>
2017-01-21 04:25:09 -0500

    remove badge

--- src/desktop/containers/TeamDetail.js
+++ src/desktop/containers/TeamDetail.js
@@ -465,7 +465,7 @@ class TeamDetail extends Component {
 				<div className="clearfix hidden-xs">
 					<header id="header" className="no-sticky" style={{background:'#fff', border:'none'}}>
 						<Sidebar 
-							badge={{chat: 1}}
+							badge={{chats: 1}}
 							padding={true}
 							menuItems={this.state.menuItems}
 							selectItem={this.selectItem.bind(this)}
