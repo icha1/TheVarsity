52c287d9c3c3ac7b3ddeaa48579564182adfdb26
fullstack360 <info@fullstack360.com>
2017-01-20 19:03:37 -0500

    updated application schema

--- models/Application.js
+++ models/Application.js
@@ -1,13 +1,21 @@
 var mongoose = require('mongoose')
 
 var ApplicationSchema = new mongoose.Schema({
-	name: {type:String, trim:true, default:''},
+	post: {type:String, trim:true, default:''},
+	from: {type:String, trim:true, default:''},
+	recipients: {type:Array, default:[]},
+	projects: {type:Array, default:[]},
+	attachments: {type:Array, default:[]},
 	timestamp: {type:Date, default:Date.now}
 })
 
 ApplicationSchema.methods.summary = function(){
 	var summary = {
-		name: this.name,
+		post: this.post,
+		from: this.from,
+		recipients: this.recipients,
+		projects: this.projects,
+		attachments: this.attachments,
 		timestamp: this.timestamp,
 		schema: 'application',
 		id: this._id.toString()
