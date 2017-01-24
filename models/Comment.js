5292e4ed8f1c687f098bf18bc02aa81427738683
fullstack360 <info@fullstack360.com>
2017-01-24 04:35:19 -0500

    more properties for comment

--- models/Comment.js
+++ models/Comment.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose')
 
 var CommentSchema = new mongoose.Schema({
 	text: {type:String, trim:true, default:''},
+	title: {type:String, trim:true, default:''},
+	url: {type:String, trim:true, default:''},
+	image: {type:String, trim:true, default:''},
 	subject: {type:String, trim:true, default:''}, // id number of post comment refers to
 	isInitial: {type:String, trim:true, default:'yes'},
 	profile: {type:mongoose.Schema.Types.Mixed, default:{}},
@@ -13,6 +16,9 @@ CommentSchema.methods.summary = function() {
 	var summary = {
 		'profile':this.profile,
 		'text':this.text,
+		'title':this.title,
+		'url':this.url,
+		'image':this.image,
 		'isInitial':this.isInitial,
 		'subject':this.subject,
 		'thread':this.thread,
