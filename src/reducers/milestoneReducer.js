a41a82e75a2d1e88193856f29284776383c89471
fullstack360 <info@fullstack360.com>
2017-01-26 06:56:44 -0500

    fetch milestones reducer action

--- src/reducers/milestoneReducer.js
+++ src/reducers/milestoneReducer.js
@@ -12,6 +12,23 @@ export default (state = initialState, action) => {
 		// 	return newState
 
 		case constants.MILESTONES_RECEIVED:
+			const keys = Object.keys(action.params)
+
+			action.milestones.forEach((milestone, i) => {
+				newState[milestone.slug] = milestone
+				newState[milestone.id] = milestone
+			})
+
+			const ignore = ['limit', 'slug', 'status']
+			for (let i=0; i<keys.length; i++){
+				let key = keys[i]
+				if (ignore.indexOf(key) != -1)
+					continue
+
+				let value = action.params[key]
+				newState[value] = action.milestones
+			}
+
 			return newState
 
 		default:
