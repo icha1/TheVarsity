04a4afbafbfed5cc8d7cf30fa9f028a54cd44ae7
fullstack360 <info@fullstack360.com>
2017-01-26 20:15:10 -0500

    context for invitations

--- models/Invitation.js
+++ models/Invitation.js
@@ -6,6 +6,7 @@ var InvitationSchema = new mongoose.Schema({
 	code: {type:String, trim:true, lowercase:true, default:''},
 	from: {type:mongoose.Schema.Types.Mixed, default:{}}, // id, email
 	team: {type:mongoose.Schema.Types.Mixed, default:{}}, // have to be invited to a team
+	context: {type:mongoose.Schema.Types.Mixed, default:{}}, // invited to a team or project
 	status: {type:String, trim:true, default:'open'},
 	timestamp: {type:Date, default:Date.now}
 })
@@ -17,6 +18,7 @@ InvitationSchema.methods.summary = function(){
 		email: this.email,
 		code: this.code,
 		team: this.team,
+		context: this.context,
 		status: this.status,
 		timestamp: this.timestamp,
 		schema: 'invitation',
