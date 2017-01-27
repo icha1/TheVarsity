657c47e6cd3c8ba35e7c4b7a598e14d6f44fe855
fullstack360 <info@fullstack360.com>
2017-01-27 03:08:22 -0500

    projects received action

---
+++ src/reducers/projectReducer.js
@@ -0,0 +1,33 @@
+import constants from '../constants/constants'
+import { project } from './initial'
+
+var initialState = Object.assign({}, project.initialState)
+
+export default (state = initialState, action) => {
+	let newState = Object.assign({}, state)
+
+	switch (action.type) {
+		case constants.PROJECTS_RECEIVED:
+			const keys = Object.keys(action.params)
+			action.projects.forEach((project, i) => {
+				newState[projects.slug] = project
+				newState[projects.id] = project
+			})
+
+			const ignore = ['limit', 'slug', 'status']
+			for (let i=0; i<keys.length; i++){
+				let key = keys[i]
+				if (ignore.indexOf(key) != -1)
+					continue
+
+				let value = action.params[key]
+				newState[value] = action.projects
+			}
+
+			return newState
+
+		default:
+			return state
+	}
+
+}
