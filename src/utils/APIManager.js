a34a87b5de84c3efc02195ede36c48c3df5fbdbd
fullstack360 <info@fullstack360.com>
2017-01-29 23:10:38 -0500

    upload zip and video to cloudinary

--- src/utils/APIManager.js
+++ src/utils/APIManager.js
@@ -1,5 +1,7 @@
 import superagent from 'superagent'
 import Promise from 'bluebird'
+import sha1 from 'sha1'
+
 
 var queue = [] // only queue the GET requests, for now
 
@@ -81,9 +83,23 @@ export default {
 	},
 
 	upload: (file, completion) => {
+		const mimeTypes = ['image', 'pdf'] 
+		let mime = null
+		mimeTypes.forEach((type, i) => { // only accepts image and pdf
+			if (file.type.indexOf(type) != -1){
+				mime = type
+			}
+		})
+
+		if (mime == null)
+			return
+
+		if (mime == 'image') // only accepts 'images' or 'pdf'
+			mime = 'images'
+
 		superagent
 		.get('https://media-service.appspot.com/api/upload')
-		.query(null)
+		.query({media: mime})
 		.set('Accept', 'application/json')
 		.end((err, res) => {
 			if (err){ 
@@ -111,11 +127,13 @@ export default {
 			    }
 
 		      	var result = resp.body.image || resp.body.pdf
+		      	if (mime == 'pdf')
+		      		result['address'] = 'https://media-service.appspot.com/site/pdf/'+result.id
+
 				completion(null, result)
 	        })
 		})
 	},
-
 
 	uploadPDF: (file, completion) => {
 		superagent
@@ -152,6 +170,54 @@ export default {
 	        })
 		})
 	},
+
+	uploadCloudinary: (file, mime, completion) => {
+		console.log('UPLOAD CLOUDINARY: '+mime)
+		if (mime != 'video' && mime != 'raw')
+			return
+		
+
+		// upload to cloudinary:
+		var cloudinaryUrl = 'https://api.cloudinary.com/v1_1/dcxaoww0c/'+mime+'/upload'
+        var uploadRequest = superagent.post(cloudinaryUrl)
+
+        uploadRequest.attach('file', file)
+
+		var timestamp = Date.now() / 1000
+		var paramsStr = 'timestamp='+timestamp+'&upload_preset=rnxsz09irVxIqxqsbdcxTo4X6bo9rUqkQms'
+
+	    uploadRequest.field('timestamp', timestamp)
+	    uploadRequest.field('api_key', '399938195648612')
+	    uploadRequest.field('upload_preset', 'rnxsz09i')
+	    uploadRequest.field('signature', sha1(paramsStr))
+
+        uploadRequest.end((err, resp) => {
+        	if (err){
+		      	console.log('UPLOAD ERROR: '+JSON.stringify(err))
+				res.json({
+					confirmation: 'fail',
+					message: err
+				})
+              	return
+        	}
+
+//			console.log('DONE == '+JSON.stringify(resp.body))
+			completion(null, resp.body)
+			// {"public_id":"gtuwtxddoqpwdhcy6tly","version":1485748482,
+			// "signature":"6b63e61d260943e64e07e87d1dc1dba4f4033ad3","width":1366,"height":768,"format":"mov",
+			// "resource_type":"video","created_at":"2017-01-30T03:54:42Z","tags":[],"pages":0,"bytes":1465764,
+			// "type":"upload","etag":"b71fd51058769437ac717a4cfe5f8c6d",
+			// "url":"http://res.cloudinary.com/dcxaoww0c/video/upload/v1485748482/gtuwtxddoqpwdhcy6tly.mov",
+			// "secure_url":"https://res.cloudinary.com/dcxaoww0c/video/upload/v1485748482/gtuwtxddoqpwdhcy6tly.mov",
+			//"audio":{"codec":"aac","bit_rate":"166689","frequency":44100,"channels":2,"channel_layout":"stereo"},
+			// "video":{"pix_format":"yuv420p","codec":"h264","level":42,"profile":"Main","bit_rate":"1511396",
+			//"dar":"683:384"},"is_audio":false,"frame_rate":60,"bit_rate":1687210,"duration":6.95,"rotation":0,
+			//"original_filename":"testvideo"}			
+
+        })
+
+	},
+
 	submitStripeToken: (token, completion) => {
 		var body = {
 			stripeToken: token.id,
